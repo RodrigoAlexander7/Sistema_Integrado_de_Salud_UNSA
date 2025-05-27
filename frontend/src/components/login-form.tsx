1014296684fe33b221bf3010ac0cbec5f5ab1a8b
RdrigoFH <139835741+RdrigoFH@users.noreply.github.com>
2025-05-27 07:27:20 -0500

    Se establece el login en español

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -18,9 +18,9 @@ export function LoginForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader>
-          <CardTitle>Login to your account</CardTitle>
+          <CardTitle>Iniciar Sesión</CardTitle>
           <CardDescription>
-            Enter your email below to login to your account
+            Bienvenido de nuevo - Inicia sesión para continuar.
           </CardDescription>
         </CardHeader>
         <CardContent>
@@ -31,36 +31,30 @@ export function LoginForm({
                 <Input
                   id="email"
                   type="email"
-                  placeholder="m@example.com"
+                  placeholder="m@ejemplo.com"
                   required
                 />
               </div>
               <div className="grid gap-3">
                 <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
+                  <Label htmlFor="password">Contraseña</Label>
                   <a
                     href="#"
                     className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
                   >
-                    Forgot your password?
+                    Olvidaste tu contraseña?
                   </a>
                 </div>
                 <Input id="password" type="password" required />
               </div>
               <div className="flex flex-col gap-3">
                 <Button type="submit" className="w-full">
-                  Login
+                  Iniciar Sesión
                 </Button>
                 <Button variant="outline" className="w-full">
-                  Login with Google
+                  Iniciar con Google
                 </Button>
               </div>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <a href="#" className="underline underline-offset-4">
-                Sign up
-              </a>
             </div>
           </form>
         </CardContent>

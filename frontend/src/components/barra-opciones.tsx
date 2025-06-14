c095755e537dc64a8eaa4bb99f260ba72460ed66
mbarrios28 <mbarriosmed@unsa.edu.pe>
2025-06-13 19:06:27 -0500

    Corrigiendo barra-opciones antes del rebase

--- frontend/src/components/barra-opciones.tsx
+++ frontend/src/components/barra-opciones.tsx
@@ -24,7 +24,7 @@ const BarraOpciones: React.FC = () => {
     const handlePerfil = (e: React.FormEvent) => {
         e.preventDefault();
         console.log("abriendo Perfil") 
-        navigate("/perfil"); 
+        navigate("/frontend/src/pages/Perfil.tsx"); 
     };
     return (
         <header className="w-full bg-white shadow-md px-4 py-3 md:px-6 flex flex-col md:flex-row items-center justify-between gap-4">
@@ -37,7 +37,7 @@ const BarraOpciones: React.FC = () => {
                 placeholder="Buscar por Código Universitario"
                 className="pl-4 pr-10 py-2 w-64 border rounded-md"
             />
-            <Button variant="ghost" size="icon" className="scale-200 hover:scale-220 transition-transform duration-200">
+            <Button variant="ghost" size="icon" className="hover:scale-110 transition-transform duration-200">
                 <FontAwesomeIcon icon={faMagnifyingGlass} style={{ color: "#1c398e" }} />
             </Button>
             </div>
@@ -45,7 +45,7 @@ const BarraOpciones: React.FC = () => {
 
         <nav className="flex items-center gap-5 w-full md:w-auto justify-end">
             
-            <Button variant="ghost" size="icon" className="scale-200 hover:scale-220 transition-transform duration-200">
+            <Button variant="ghost" size="icon" className="hover:scale-110 transition-transform duration-200">
             
                 <Tooltip>
                     <TooltipTrigger><FontAwesomeIcon icon={faHouse} style={{ color: "#1c398e" }} /></TooltipTrigger>
@@ -54,7 +54,7 @@ const BarraOpciones: React.FC = () => {
                     </TooltipContent>
                 </Tooltip>
             </Button>
-            <Button variant="ghost" size="icon" className="scale-200 hover:scale-220 transition-transform duration-200">
+            <Button variant="ghost" size="icon" className="hover:scale-110 transition-transform duration-200">
             
                 <Tooltip>
                     <TooltipTrigger><FontAwesomeIcon icon={faFileLines} style={{ color: "#1c398e" }} /></TooltipTrigger>
@@ -63,7 +63,7 @@ const BarraOpciones: React.FC = () => {
                     </TooltipContent>
                 </Tooltip>
             </Button>
-            <Button variant="ghost" size="icon" className=" scale-200 hover:scale-220 transition-transform duration-200">
+            <Button variant="ghost" size="icon" className="hover:scale-110 transition-transform duration-200">
             
                 <Tooltip>
                     <TooltipTrigger><FontAwesomeIcon icon={faUserCheck} style={{ color: "#1c398e" }} /></TooltipTrigger>
@@ -72,7 +72,7 @@ const BarraOpciones: React.FC = () => {
                     </TooltipContent>
                 </Tooltip>
             </Button>
-            <Button variant="ghost" size="icon" className="scale-200 hover:scale-220 transition-transform duration-200">
+            <Button variant="ghost" size="icon" className="hover:scale-110 transition-transform duration-200">
             
                 <Tooltip>
                     <TooltipTrigger><FontAwesomeIcon icon={faUserPlus} style={{ color: "#1c398e" }} /></TooltipTrigger>
@@ -81,7 +81,7 @@ const BarraOpciones: React.FC = () => {
                     </TooltipContent>
                 </Tooltip>
             </Button>
-            <Button variant="ghost" size="icon" className="scale-200 hover:scale-220 transition-transform duration-200" onClick={handlePerfil}>
+            <Button variant="ghost" size="icon" className="hover:scale-110 transition-transform duration-200" onClick={handlePerfil}>
                 
                 <Tooltip>
                     <TooltipTrigger><FontAwesomeIcon icon={faCircleUser} className="h-6 w-6"style={{ color: "#1c398e" }}/></TooltipTrigger>

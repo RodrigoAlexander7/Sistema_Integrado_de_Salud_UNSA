10118f262bc345874989275694c84dc8eb4826cb
RodryAlexander <rodrygoleu7@gmail.com>
2025-06-23 13:24:05 -0500

    Documentacion de /config

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '../generated/prisma';
 import { config } from './environment';
 
+// Creamos una istancia global para poder usar prisma desde cualquier parte del proyecto y creamos los logs
 export const prisma = new PrismaClient({
   datasources: {
     db: {
